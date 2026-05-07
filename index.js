2c87671314e5542c31d2861e551df8fcf2589e1a
Imanol Mollehuanca <imanol.mollehuanca@tecsup.edu.pe>
2026-05-07 10:24:44 -0500

    Agregamos con estilos

--- index.js
+++ index.js
@@ -4,20 +4,42 @@ const fs = require('fs')
 const path = require('path')
 
 function requestController(req, res) {
+    if(req.url === '/') {
 
-    const filePath = path.join(__dirname, 'public', 'index.html')
+        const filePath = path.join(__dirname, 'public', 'index.html')
 
-    fs.readFile(filePath, (err, content) => {
+        fs.readFile(filePath, (err, content) => {
 
-        if(err){
-            res.writeHead(500)
-            res.end('Error cargando archivo')
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(content)
-        }
+            if(err){
+                res.writeHead(500)
+                res.end('Error cargando archivo')
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.end(content)
+            }
 
-    })
+        })
+
+    }
+
+    // CSS
+    else if(req.url === '/style.css') {
+
+        const filePath = path.join(__dirname, 'public', 'style.css')
+
+        fs.readFile(filePath, (err, content) => {
+
+            if(err){
+                res.writeHead(500)
+                res.end('Error cargando CSS')
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/css'})
+                res.end(content)
+            }
+
+        })
+
+    }
 
 }
 

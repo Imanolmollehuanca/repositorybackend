beb8127618152af0c47ccddfe6be2181983334a9
Imanol Mollehuanca <imanol.mollehuanca@tecsup.edu.pe>
2026-05-07 10:15:50 -0500

    Agrgamos frontend

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 require('dotenv').config()
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
-function requestController() {
-    console.log('Bienvenidos al curso')
+function requestController(req, res) {
+
+    const filePath = path.join(__dirname, 'public', 'index.html')
+
+    fs.readFile(filePath, (err, content) => {
+
+        if(err){
+            res.writeHead(500)
+            res.end('Error cargando archivo')
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end(content)
+        }
+
+    })
+
 }
 
 const server = http.createServer(requestController)
